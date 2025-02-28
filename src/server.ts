a6143b04c418dd29224f0c2125c6a8a68f5b3437
Diogo  Alberto <kirchoffalberto@gmail.com>
2025-02-28 00:30:27 -0300

    ✨feat: initial commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response, json} from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ player: "Beckham"});
+})
+
+app.listen(port, () => {
+    console.log(`🚀 Server running at port http://localhost:${port}`);
+})
