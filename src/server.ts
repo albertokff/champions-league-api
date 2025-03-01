b7e65a7ac536039c8c203289533a20099074a38a
Diogo  Alberto <kirchoffalberto@gmail.com>
2025-02-28 22:48:23 -0300

    ✨ Update

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import express, {Request, Response, json} from "express";
+import createApp from "./app";
 
-const app = express();
+const app = createApp();
 const port = process.env.PORT;
-
-app.use(json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({ player: "Beckham"});
-})
 
 app.listen(port, () => {
     console.log(`🚀 Server running at port http://localhost:${port}`);
